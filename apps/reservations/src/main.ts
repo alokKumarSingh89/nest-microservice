092288d5df4cc3f32f178837500cb8cec8087bc0
alokKumarSingh89 <itsaloksingh2011@gmail.com>
2026-03-28 23:57:17 +0530

    feat(auth): implement basic authentication module with controller and service
    - Add AuthController with a simple GET endpoint returning "Hello World!"
    - Create AuthService to handle business logic for authentication
    - Implement unit tests for AuthController
    - Set up end-to-end tests for AuthController
    - Add AuthModule to encapsulate authentication-related components
    - Create main entry point for the application
    - Add Dockerfile for building and running the application in development and production environments
    - Introduce logger module using nestjs-pino for structured logging
    - Add .dockerignore to exclude node_modules and dist directories
    - Configure TypeScript for the auth application

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
+  console.log('Starting Reservations Service...');
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
