3c56a6f42b70433de83e8d24b0081ac79ca00cc5
alokKumarSingh89 <itsaloksingh2011@gmail.com>
2026-03-29 00:17:44 +0530

    feat(auth): implement user management module with user creation and validation

---
+++ apps/auth/src/users/models/user.schema.ts
@@ -0,0 +1,12 @@
+import { AbstractDocument } from '@app/common';
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+
+export class UserDocument extends AbstractDocument {
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(UserDocument);
