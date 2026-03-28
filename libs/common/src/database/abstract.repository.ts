7a94dc0a5dcd805a4baeea4b899871f4576b5848
alokKumarSingh89 <itsaloksingh2011@gmail.com>
2026-03-28 17:47:21 +0530

    feat: restructure application into reservations microservice with database integration and configuration module

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,60 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { AbstractSchema } from './abstract.schema';
+import { Model, Types, QueryFilter, UpdateQuery } from 'mongoose';
+
+export abstract class AbstractRepository<TDocument extends AbstractSchema> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (await createDocument.save()).toJSON();
+  }
+
+  async findOne(filterQuery: QueryFilter<TDocument>): Promise<TDocument> {
+    const document = await this.model
+      .findOne(filterQuery)
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn(
+        `Document not found with filter: ${JSON.stringify(filterQuery)}`,
+      );
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: QueryFilter<TDocument>,
+    updateData: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, updateData, { new: true })
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn(
+        `Document not found with filter: ${JSON.stringify(filterQuery)}`,
+      );
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async find(filterQuery: QueryFilter<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery).lean<TDocument[]>(true);
+  }
+
+  async findOneAndDelete(
+    filterQuery: QueryFilter<TDocument>,
+  ): Promise<TDocument | null> {
+    return this.model.findOneAndDelete(filterQuery).lean<TDocument>(true);
+  }
+}
