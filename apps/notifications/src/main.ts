f266c74cae6c4a770e783fa4582907949b632241
alokKumarSingh89 <itsaloksingh2011@gmail.com>
2026-03-31 16:48:25 +0530

    feat(notifications): set up notifications microservice with TCP transport and configuration validation

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationsModule } from './notifications.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+  app.useLogger(app.get(Logger));
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get<number>('PORT')!,
+    },
+  });
+  await app.startAllMicroservices();
 }
 bootstrap();
--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        PORT: joi.number().required(),
+      }),
+    }),
+
+    LoggerModule,
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
